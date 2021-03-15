c1657bed9d425aae27ce544c20eeb6a3dda7f873
redwrasse <redwrasse@users.noreply.github.com>
2021-03-14 18:58:06 -0700

    working http handler, but strange excess prefix completions on war and peace

--- appgo/main.go
+++ appgo/main.go
@@ -1,8 +1,75 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+type HttpTrieHandler struct {
+	t Trie
+}
+
+
+func InitializeTrieHandler() HttpTrieHandler {
+	trieHandler := HttpTrieHandler{MakeNew()}
+	MakeWarAndPeaceTrie(trieHandler.t)
+	return trieHandler
+}
+
+func (HTH HttpTrieHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	// expects url /?prefix=<prefix>
+	fmt.Println("GET params: ", r.URL.Query())
+
+	prefix := r.URL.Query().Get("prefix")
+
+	p := CleanLine(string(prefix))
+	fmt.Println("Read prefix: ", p)
+	fmt.Println("Autocompleting on %s", p)
+	completions := HTH.t.AutoCompletions(p)
+	//fmt.Printf("%v\n", completions)
+	json.NewEncoder(w).Encode(completions)
+	fmt.Println("called autocomplete handler, returning %d completions", len(completions))
+}
 
 func main() {
+
+	trieHandler := InitializeTrieHandler()
+	fmt.Printf("%v\n", trieHandler.t.AutoCompletions("zzzzzzzz"))
+	//log.Fatal(http.ListenAndServe(":8080", trieHandler))
+
+}
+
+func MakeWarAndPeaceTrie(t Trie) {
+
+	file, err := os.Open("./war_and_peace.txt")
+	if err != nil { log.Fatal(err) }
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+
+		s := CleanLine(scanner.Text())
+		if len(s) > 0 {
+			//fmt.Println(s)
+			t.Insert(s)
+		}
+	}
+
+	if err := scanner.Err(); err != nil { log.Fatal(err) }
+
+}
+
+func CleanLine(s string) string {
+	return strings.ToLower(strings.Join(strings.Fields(strings.TrimSpace(s)), " "))
+}
+
+func ExampleTrie() {
 
 	t := MakeNew()
 	t.Insert("foo")
@@ -14,4 +81,3 @@ func main() {
 	fmt.Printf("%v\n", t.AutoCompletions("fo"))
 
 }
-
